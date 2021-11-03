31fd6fee09e06dbb162b614eb2748b37b86877da
harold Pierrache <h.pierrache@gmail.com>
2021-11-03 16:46:34 +0100

    devjs

--- js/memory.js
+++ js/memory.js
@@ -2,14 +2,49 @@ let cards = document.getElementsByClassName("card");
 
 let colors = ["red", "yellow", "green", "blue", "orange", "purple"];
 
-var randomColor = colors[Math.floor(Math.random()*colors.length)];
+let randomColor = colors[Math.floor(Math.random()*colors.length)];
 
 
-function cardColors(){
-   for(const card of cards) {
-    card.classList.toggle(randomColor);
-       console.log(card);
-   }
+cards[0].onclick = function () { 
+        this.classList.toggle("red");
+    }
+cards[1].onclick = function () {
+        this.classList.toggle("green");    
 }
+cards[2].onclick = function () {
+    this.classList.toggle("blue");    
+}
+cards[3].onclick = function () {
+    this.classList.toggle("orange");    
+}
+cards[4].onclick = function () {
+    this.classList.toggle("purple");    
+}
+cards[5].onclick = function () {
+    this.classList.toggle("green");    
+}
+cards[6].onclick = function () {
+    this.classList.toggle("yellow");    
+}
+cards[7].onclick = function () {
+    this.classList.toggle("red");    
+}
+cards[8].onclick = function () {
+    this.classList.toggle("orange");    
+}
+cards[9].onclick = function () {
+    this.classList.toggle("blue");    
+}
+cards[10].onclick = function () {
+    this.classList.toggle("purple");    
+}
+cards[11].onclick = function () {
+    this.classList.toggle("yellow");    
+}
+    
+    
+ 
+
+
 
 
