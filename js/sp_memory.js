fec8449c7bd66c8e0283485bfff5e526ed347fa8
harold Pierrache <h.pierrache@gmail.com>
2021-11-04 11:26:32 +0100

    south_park_memory

---
+++ js/sp_memory.js
@@ -0,0 +1,39 @@
+let hiddenCards = document.getElementsByClassName("hiddenCard");
+
+
+hiddenCards[0].onclick = function() {
+    hiddenCards[0].classList.toggle("opacity-0");
+}
+hiddenCards[1].onclick = function() {
+    hiddenCards[1].classList.toggle("opacity-0");
+}
+hiddenCards[2].onclick = function() {
+    hiddenCards[2].classList.toggle("opacity-0");
+}
+hiddenCards[3].onclick = function() {
+    hiddenCards[3].classList.toggle("opacity-0");
+}
+hiddenCards[4].onclick = function() {
+    hiddenCards[4].classList.toggle("opacity-0");
+}
+hiddenCards[5].onclick = function() {
+    hiddenCards[5].classList.toggle("opacity-0");
+}
+hiddenCards[6].onclick = function() {
+    hiddenCards[6].classList.toggle("opacity-0");
+}
+hiddenCards[7].onclick = function() {
+    hiddenCards[7].classList.toggle("opacity-0");
+}
+hiddenCards[8].onclick = function() {
+    hiddenCards[8].classList.toggle("opacity-0");
+}
+hiddenCards[9].onclick = function() {
+    hiddenCards[9].classList.toggle("opacity-0");
+}
+hiddenCards[10].onclick = function() {
+    hiddenCards[10].classList.toggle("opacity-0");
+}
+hiddenCards[11].onclick = function() {
+    hiddenCards[11].classList.toggle("opacity-0");
+}
